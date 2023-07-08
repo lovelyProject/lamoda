3df079c61d124dc3f000bb962a736a583e9fc7bc
Edward <edward.hardwork2000@gmail.com>
2023-07-08 10:15:32 +0500

    Настроить получение данных с апи

--- src/pages/products/index.vue
+++ src/pages/products/index.vue
@@ -1,3 +1,23 @@
 <template lang="pug">
-div 123
+div {{ filterParam }}
 </template>
+
+<script>
+//state
+import { actionTypes } from "@/store/product";
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState({
+      products: (state) => state.product.products
+    }),
+    filterParam() {
+      return this.$route.query.filter;
+    }
+  },
+  mounted() {
+    this.$store.dispatch(actionTypes.getProducts)
+  },
+}
+</script>
--- src/store/product/index.js
+++ src/store/product/index.js
@@ -1,4 +1,50 @@
+import productsApi from "@/api/products.js"
+
 const state = {
-    products: []
+    products: [],
+    isLoading: false,
+    isError: false,
+    filterVars: ["men\'s clothing", "women\'s clothing"]
 }
-export default {state}
+export const mutationTypes = {
+    getProductsStart: "[products] get products start",
+    getProductsSuccess: "[products] get products success",
+    getProductsFailure: "[products] get products failure",
+}
+
+const mutations = {
+    [mutationTypes.getProductsStart]: (state) => {
+        state.isLoading = true;
+    },
+    [mutationTypes.getProductsSuccess]: (state, payload) => {
+        state.products = payload;
+        state.isLoading = false;
+    },
+    [mutationTypes.getProductsFailure]: (state) => {
+        state.isLoading = false;
+        state.isError = true;
+    }
+}
+export const actionTypes = {
+    getProducts: "[products] get products",
+};
+
+const actions = {
+    [actionTypes.getProducts](context) {
+        return new Promise((resolve) => {
+            context.commit(mutationTypes.getProductsStart);
+            productsApi.getProducts()
+                .then((response) => {
+                    const data = response.data;
+                    const filterData = data.filter((item) => state.filterVars.includes(item.category));
+
+                    context.commit(mutationTypes.getProductsSuccess, filterData);
+                    resolve(response.data);
+                })
+                .catch((error) => {
+                    console.log("can\'t get products", error);
+                })
+        })
+    }
+}
+export default { state, actions, mutations, actionTypes, mutationTypes }
